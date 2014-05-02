67dc55c52bacca9989d7abcbdb331d0116d3f004
ikoddn <ikonage@gmail.com>
2014-05-03 00:30:13 +0200

    Skybox: Replaced the sun sphere with a textured quad

--- src/nodes/Skybox.cpp
+++ src/nodes/Skybox.cpp
@@ -1,9 +1,9 @@
 #include <memory>
+#include "components/Blending.h"
 #include "components/DisableDepthMasking.h"
 #include "components/IBODraw.h"
-#include "components/SimpleDraw.h"
 #include "mesh/Cube.h"
-#include "mesh/Sphere.h"
+#include "mesh/Quad.h"
 #include "nodes/MeshNode.h"
 #include "nodes/Skybox.h"
 #include "LightProperties.h"
@@ -18,27 +18,38 @@ using namespace std;
 Skybox::Skybox(const ShaderProgram& skyboxShader, const ShaderProgram& lightShader, const shared_ptr<TextureCubeMap>& texture) {
 
 	shared_ptr<Cube> cubeMesh(make_shared<Cube>(true));
-	shared_ptr<Sphere> sphereMesh(make_shared<Sphere>());
+	shared_ptr<Quad> quadMesh(make_shared<Quad>());
+
+	shared_ptr<IBODraw> iboDraw(make_shared<IBODraw>());
+	shared_ptr<DisableDepthMasking> disableDepthMasking(make_shared<DisableDepthMasking>());
+	shared_ptr<Blending> blending(make_shared<Blending>());
 
 	shared_ptr<MeshNode> cubeNode(make_shared<MeshNode>(skyboxShader, cubeMesh));
 
-	cubeNode->addComponent(make_shared<IBODraw>());
-	cubeNode->addComponent(make_shared<DisableDepthMasking>());
+	cubeNode->addComponent(iboDraw);
+	cubeNode->addComponent(disableDepthMasking);
 	cubeNode->addTexture(texture);
 
 	std::string resourceBase(OGF_RESOURCE_DIR);
 	string textureDir = resourceBase + "/textures/";
 	shared_ptr<Texture> glow(make_shared<Texture2D>(textureDir + "glow.png"));
 	shared_ptr<Texture> sky(make_shared<Texture2D>(textureDir + "sky.png"));
+	shared_ptr<Texture> sun(make_shared<Texture2D>(textureDir + "sun.png"));
 
 	cubeNode->addTexture(sky);
 	cubeNode->addTexture(glow);
 
 	sunLightNode = make_shared<LightNode>(LightProperties::SUNLIGHT);
+	sunLightNode->move(10000.0f, 0.0f, 0.0f);
+	sunLightNode->disableParentTransformation(AbstractNode::ROTATION_SELF);
 
-	sunNode = make_shared<MeshNode>(lightShader, sphereMesh);
-	sunNode->addComponent(make_shared<SimpleDraw>());
-	sunNode->move(200.0f, 0.0f, 0.0f);
+	sunNode = make_shared<MeshNode>(lightShader, quadMesh);
+	sunNode->addComponent(iboDraw);
+	sunNode->addComponent(disableDepthMasking);
+	sunNode->addComponent(blending);
+	sunNode->addTexture(sun);
+	sunNode->move(5.0f, 0.0f, 0.0f);
+	sunNode->rotateAroundSelfY(-90.0f);
 	sunNode->add(sunLightNode);
 
 	children.push_back(cubeNode);
